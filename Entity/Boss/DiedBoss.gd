f998f400255441562f951fcb3c94171a042a50d0
Rinka <76248335+RinkaWai0210@users.noreply.github.com>
2024-05-12 05:41:03 +0800

    Boss

---
+++ Entity/Boss/DiedBoss.gd
@@ -0,0 +1,12 @@
+class_name DiedBoss
+extends State
+
+@export var enemyHP : int = 100
+
+func enter() -> void:
+	super()
+	await get_tree().create_timer(1).timeout
+	parent.queue_free()
+
+func chase_player(delta: float, direction: int)->void:
+	parent.queue_free()
