1202e917ca9ab794dbc482b2fe63ed31b350060b
Rinka <76248335+RinkaWai0210@users.noreply.github.com>
2024-05-11 12:31:41 +0800

    ver.Enemy.2

--- Entity/Enemy/Enemy.gd
+++ Entity/Enemy/Enemy.gd
@@ -1,6 +1,10 @@
 extends Node
 
+@onready var animation = $Animation
+
 @export var enemyAttack: int = 10
+const enemySpeed: int = 10
+var enemyDirection = Vector2.RIGHT
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
